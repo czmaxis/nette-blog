5ba0e82ea350b6d492403a2f7a4aea4e1d1be1cb
czmaxis <czmaxis55@gmail.com>
2025-09-21 20:12:52 +0200

    přidávání komentářů

--- app/Presentation/Post/PostPresenter.php
+++ app/Presentation/Post/PostPresenter.php
@@ -20,8 +20,43 @@ public function renderShow(int $id): void
 	}
 
 	$this->template->post = $post;
+	$this->template->comments = $post->related('comments')->order('created_at');
+}
+
+protected function createComponentCommentForm(): Form
+{
+	$form = new Form; // means Nette\Application\UI\Form
+
+	$form->addText('name', 'Jméno:')
+		->setRequired();
+
+	$form->addEmail('email', 'E-mail:');
+
+	$form->addTextArea('content', 'Komentář:')
+		->setRequired();
+
+	$form->addSubmit('send', 'Publikovat komentář');
+
+	$form->onSuccess[] = $this->commentFormSucceeded(...);
+	
+	 return $form;
+}
+
+private function commentFormSucceeded(\stdClass $data): void
+{
+	$id = $this->getParameter('id');
+
+	$this->database->table('comments')->insert([
+		'post_id' => $id,
+		'name' => $data->name,
+		'email' => $data->email,
+		'content' => $data->content,
+	]);
+
+	$this->flashMessage('Děkuji za komentář', 'success');
+	$this->redirect('this');
 }
 
 
-    
+
 }

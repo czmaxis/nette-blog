98feca9ce24b86dd69a66519e7a8a686807ceda4
czmaxis <czmaxis55@gmail.com>
2025-09-22 18:46:24 +0200

    přidávání nových příspěvků

---
+++ app/Presentation/Edit/EditPresenter.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Presentation\Edit;
+
+use Nette;
+use Nette\Application\UI\Form;
+
+final class EditPresenter extends Nette\Application\UI\Presenter
+{
+	public function __construct(
+		private Nette\Database\Explorer $database,
+	) {
+	}
+
+    protected function createComponentPostForm(): Form
+{
+	$form = new Form;
+	$form->addText('title', 'Titulek:')
+		->setRequired();
+	$form->addTextArea('content', 'Obsah:')
+		->setRequired();
+
+	$form->addSubmit('send', 'Uložit a publikovat');
+	$form->onSuccess[] = $this->postFormSucceeded(...);
+
+	return $form;
+}
+private function postFormSucceeded(\stdClass $data): void
+{
+    $post = $this->database
+    ->table('posts')
+    ->insert($data);
+
+    $this->flashMessage('Příspěvek byl uložen a publikován', 'success');
+    $this->redirect('Post:show', $post->id);
+}
+}
